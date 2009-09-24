3f17347987066cb9820cccc01c9977d7baa7bcbf
Andreas Gies <andreas@soa-knowledge.net>
2009-09-24 17:00:53 +0200

    Documentation fixes

--- webgen/ext/init.rb
+++ webgen/ext/init.rb
@@ -1,18 +1,3 @@
-# Copyright (C) 2009, Progress Software Corporation and/or its
-# subsidiaries or affiliates.  All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 # = webgen extensions directory
 #
 # All init.rb files anywhere under this directory get automatically loaded on a webgen run. This
@@ -22,7 +7,6 @@
 #
 # The +config+ variable below can be used to access the Webgen::Configuration object for the current
 # website.
-
 $LOAD_PATH << File.dirname(__FILE__)
 load 'fuse/asciidoc.rb'
 load 'fuse/includeThumbs.rb'
